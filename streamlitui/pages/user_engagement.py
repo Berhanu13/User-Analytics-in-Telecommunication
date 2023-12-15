f3d0d32b6f9c0f341a694e9d71615a12cbdc508e
Naol Lamesa <malkaamedia@gmail.com>
2023-12-15 22:27:11 +0300

    modification

--- streamlitui/pages/user_engagement.py
+++ streamlitui/pages/user_engagement.py
@@ -5,7 +5,7 @@ import pickle
 
 cwd = os.getcwd()
 
-@st.cache_data
+@st.cache
 def load_data(DATA_URL):
     data = pickle.load(open(DATA_URL, "rb"))
     return data
@@ -13,7 +13,7 @@ def load_data(DATA_URL):
 st.set_page_config(page_title="User Engagement Analysis", page_icon="👤", layout="wide")
 
 st.markdown("## User Engagement")
-st.markdown("### In this task, each users' engagement was studied")
+st.markdown("### In this task, each user's engagement was studied")
 st.markdown("""
 - Top ten users by session traffic: which is Downloaded and Uploaded data
 - Top ten users per sessions frequency
@@ -38,9 +38,10 @@ st.markdown("### Top ten customers with longest session ")
 st.table(data['top_ten_per_duration'])
 st.markdown("# ")
 
-df1 = pd.DataFrame(data['top_ten_per_traffic'])
-df2 = pd.DataFrame(data['top_ten_per_freq'])
-df3 = pd.DataFrame(data['top_ten_per_duration'])
+# Convert dictionary values to DataFrames
+df1 = pd.DataFrame.from_dict(data['top_ten_per_traffic'], orient='index')
+df2 = pd.DataFrame.from_dict(data['top_ten_per_freq'], orient='index')
+df3 = pd.DataFrame.from_dict(data['top_ten_per_duration'], orient='index')
 
 # Find the common index among all DataFrames
 common_index = df1.index.intersection(df2.index).intersection(df3.index)
