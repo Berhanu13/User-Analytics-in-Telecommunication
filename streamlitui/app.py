15c40dc5269d603393ceda7ff30f340f27b4c6a2
Naol Lamesa <malkaamedia@gmail.com>
2023-12-15 20:20:18 +0300

    modification

--- streamlitui/app.py
+++ streamlitui/app.py
@@ -11,13 +11,16 @@ st.set_page_config(page_title="Telecom Analytics", layout="wide")
 
 # st.sidebar.markdown("Please select the desired page")
 
+image_path = f"{cwd}/images/10acc.png"
+st.image(image_path, caption='10 Academy', use_column_width=True)
+
+
 st.markdown("""
 
-    ## TelCo Telecom Data Analysis
-    > The week 1 challenge of [10Academy](https://www.10academy.org/) is to provide a report to analyse opportunities for growth and make a recommendation on whether TellCo is worth buying or selling.  This is done by analysing a telecommunication dataset that contains useful information about the customers & their activities on the network. It is required to deliver insights via web based dashboard and a written report.
+    ## User Analytics in the Telecommunication Industry
+    > The investor, who's good at finding valuable deals, wants to check out TellCo, a mobile company in Pefkakia. I am diving into TellCo's data to see if it's a good buy or not. I am focusing on the info the company's machines automatically collect. The goal is to find chances for growth and figure out if it's a smart move to buy or sell TellCo. I am going to use what I learn from a bunch of telecom data to make decisions that fit the changing telecom world. This report keeps it simple while I dig into the numbers to see what makes sense for the investor.
     ## Data
-    - The data extracted from a month of aggregated data on xDR. 
-    - The features described can be found below
+    > For this project, I used data from a monthly aggregation of xDR records. I obtained a data dump, which was then loaded into a Postgres database. The dataset comprises 55 columns and 15,001 rows.
 """)
 
 df=pd.read_csv(f"{cwd}/streamlitui/asset/st.csv")
